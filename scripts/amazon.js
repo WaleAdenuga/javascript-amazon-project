47362cdb7203f424732521ea424e1779c9ab72af
Wale Adenuga <adewaleadenuga56@gmail.com>
2024-05-30 23:09:40 +0200

    Used product.js

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -5,42 +5,12 @@
 // make it interactive
 
 // use an object to represent each product since you can group multiple values together
-const products = [
-    {
-        image: 'images/products/athletic-cotton-socks-6-pairs.jpg',
-        name: 'Black and Gray Athletic Cotton Socks - 6 Pairs',
-        rating: {
-            stars: 4.5,
-            count: 87
-        },
-        priceCents: 1090,
-    },
-
-    {
-        image: 'images/products/intermediate-composite-basketball.jpg',
-        name: 'Intermediate Size Basketball',
-        rating: {
-            stars: 4,
-            count: 127
-        },
-        priceCents: 2095,    
-    },
-    {
-        image: 'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
-        name: 'Adults Plain Cotton T-Shirt - 2 Pack',
-        rating: {
-            stars: 4.5,
-            count: 56
-        },
-        priceCents: 799,
-    }
-
-];
 
 let productsHTML = '';
 
 // recall foreach takes each object in array, saves in product and then runs the function
 // we want to create some html for each object
+// benefit of generating html is you can just add another object to the array without duplicating html code
 products.forEach((product) => {
     // accumulator pattern
     productsHTML += `
